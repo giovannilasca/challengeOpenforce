fc2d81dd95661dd9a6136aa165d5f45a27fed912
savag3s1nuso1d <lascagiovanni@outlook.it>
2020-12-21 21:25:26 +0100

    Task1, prima bozza

--- Task1.py
+++ Task1.py
@@ -1,1 +1,11 @@
 import csv
+
+file = open("harvardMIT.csv", "rt")
+myReader = csv.reader(file)
+
+instructor = input("Inserisci il nome di un professore per visualizzarne i corsi: ")
+for row in myReader:
+    if instructor == '':
+        print(row[1] + ', ' + row[3] + ', ' + row[0])
+    if instructor.lower().strip() in row[4].lower():
+        print(row[1] + ', ' + row[3] + ', ' + row[0])
