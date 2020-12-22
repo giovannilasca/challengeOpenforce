38e3bd640b0a147a42d86e848da8599d7f6dd9d0
savag3s1nuso1d <lascagiovanni@outlook.it>
2020-12-22 19:14:49 +0100

    task1 aggiornato

--- Task1.py
+++ Task1.py
@@ -1,11 +1,26 @@
 import csv
+import urllib.request as req
 
-file = open("harvardMIT.csv", "rt")
-myReader = csv.reader(file)
+def main():
+    print('1. Scegliere il file' )
+    answer = input('Si desidera aprire un file presente in locale o da un particolare indirizzo url? Digitare "u" per la prima opzione, "l" per la seconda: ')
+    if answer == 'u':
+        url = input('Inserire l\'indirizzo url del file: ')
+        with req.urlopen(url, 't') as file:
+            #completare
 
-instructor = input("Inserisci il nome di un professore per visualizzarne i corsi: ")
-for row in myReader:
-    if instructor == '':
-        print(row[1] + ', ' + row[3] + ', ' + row[0])
-    if instructor.lower().strip() in row[4].lower():
-        print(row[1] + ', ' + row[3] + ', ' + row[0])
+    elif answer == 'l':
+        path = input('Inserire il percorso del file: ')
+        file = open(path, "rt")
+        data = csv.reader(file)
+        instructor = input("Inserisci il nome di un professore per visualizzarne i corsi: ")
+        for row in data:
+            if instructor == '':
+                print(row[1] + ', ' + row[3] + ', ' + row[0])
+            elif instructor.lower().strip() in row[4].lower():
+                print(row[1] + ', ' + row[3] + ', ' + row[0])
+    else:
+        print('Solo "u" o "l" sono risposte valide!')
+        main()
+
+main()
