d3e151b8874191c274d1a61ce7f66e2d016dd8bc
savag3s1nuso1d <lascagiovanni@outlook.it>
2020-12-22 22:52:50 +0100

    Aggiornamento task 1, e versione 1.2

--- Task1.2.py
+++ Task1.2.py
@@ -2,6 +2,16 @@ import csv
 import unicodecsv
 import urllib.request as req
 
+# Questa è una versione del primo task con i metodi definiti anche per il file preso dall'url, ma purtroppo non funziona in quanto dopo aver passato il nome del 
+# professore per filtrare non printa i risultati (non riesco a trovare una soluzione purtroppo ma è così che avrei voluto organizzare il codice di questo task
+# quindi allego anche questo file)
+
+def url_method():
+    url = input('Inserire l\'indirizzo url del file: ')
+    with req.urlopen(url) as file:
+        data = unicodecsv.reader(file, encoding='utf-8-sig', delimiter=',')
+    return data
+
 def local_method():
     path = input('Inserire il percorso del file: ')
     file = open(path, "rt")
@@ -20,15 +30,7 @@ def main():
     print('Scegliere il file .csv da leggere')
     answer = input('Si desidera aprire un file presente in locale o da un particolare indirizzo url? Digitare "l" per la prima opzione, "u" per la seconda: ')
     if answer == 'u':
-        url = input('Inserire l\'indirizzo url del file: ')
-        with req.urlopen(url) as file:
-            data = unicodecsv.reader(file, encoding='utf-8-sig', delimiter=',')
-            instructor = input("Inserisci il nome di un professore per visualizzarne i corsi: ")
-            for row in data:
-                if instructor == '':
-                    print(row[1] + ', ' + row[3] + ', ' + row[0])
-                elif instructor.lower().strip() in row[4].lower():
-                    print(row[1] + ', ' + row[3] + ', ' + row[0]) 
+        filter_method(url_method()) 
     elif answer == 'l':  
         filter_method(local_method())
     else:
