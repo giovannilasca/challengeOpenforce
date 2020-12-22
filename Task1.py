d1fdba4894f243e85f753b5938c531138a845c18
savag3s1nuso1d <lascagiovanni@outlook.it>
2020-12-22 22:00:53 +0100

    organizzazione in metodi dedicati

--- Task1.py
+++ Task1.py
@@ -1,24 +1,34 @@
 import csv
+import unicodecsv
 import urllib.request as req
 
-def main():
-    print('1. Scegliere il file' )
-    answer = input('Si desidera aprire un file presente in locale o da un particolare indirizzo url? Digitare "u" per la prima opzione, "l" per la seconda: ')
-    if answer == 'u':
-        url = input('Inserire l\'indirizzo url del file: ')
-        with req.urlopen(url, 't') as file:
-            #completare
+def url_method():
+    url = input('Inserire l\'indirizzo url del file: ')
+    with req.urlopen(url) as file:
+        data = unicodecsv.reader(file, encoding='utf-8-sig', delimiter=',')
+    return data
 
-    elif answer == 'l':
-        path = input('Inserire il percorso del file: ')
-        file = open(path, "rt")
-        data = csv.reader(file)
-        instructor = input("Inserisci il nome di un professore per visualizzarne i corsi: ")
-        for row in data:
-            if instructor == '':
-                print(row[1] + ', ' + row[3] + ', ' + row[0])
-            elif instructor.lower().strip() in row[4].lower():
-                print(row[1] + ', ' + row[3] + ', ' + row[0])
+def local_method():
+    path = input('Inserire il percorso del file: ')
+    file = open(path, "rt")
+    data = csv.reader(file)
+    return data
+
+def filter_method(data):
+    instructor = input("Inserisci il nome di un professore per visualizzarne i corsi: ")
+    for row in data:
+        if instructor == '':
+            print(row[1] + ', ' + row[3] + ', ' + row[0])
+        elif instructor.lower().strip() in row[4].lower():
+            print(row[1] + ', ' + row[3] + ', ' + row[0])
+
+def main():
+    print('Scegliere il file .csv da leggere')
+    answer = input('Si desidera aprire un file presente in locale o da un particolare indirizzo url? Digitare "l" per la prima opzione, "u" per la seconda: ')
+    if answer == 'u':
+        filter_method(url_method()) 
+    elif answer == 'l':  
+        filter_method(local_method())
     else:
         print('Solo "u" o "l" sono risposte valide!')
         main()
