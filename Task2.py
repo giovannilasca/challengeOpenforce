69d92ded097b4433243eeb3cca27bea59b481ff4
savag3s1nuso1d <lascagiovanni@outlook.it>
2020-12-24 15:39:29 +0100

    Corretti alcuni errori e aggiunte funzioni filtro

--- Task2.py
+++ Task2.py
@@ -13,31 +13,54 @@ def local_method():
     file = open(path)
     data = csv.reader(file)
     return data
-    
+
 def filter_method(data):
     instructors = []
     instructor = input('Inserisci il nome di un professore per visualizzarne i corsi o premere Invio per visualizzarli tutti: ')
     if instructor == '':
-        remove_dup(data)
-        for row in dup_free:
-            print(row[1] + ', ' + row[3] + ', ' + row[0])
-    elif instructor != '':
-        instructors.append(instructor)
-        while instructor.lower() != 'q':
-            instructor = input('Vuoi inserire il nome di un altro professore? Inserire un altro nome oppure "q" per andare avanti: ')
-            instructors.append(instructor)
         for row in data:
-            for instructor in instructors:    
-                if instructor.lower().strip() in row[4].lower():
-                    print(row[1] + ', ' + row[3] + ', ' + row[0])
+            institution = row[0]
+            course_number = row[1]
+            launch_date = row[2]
+            course_title = row[3]
+            teachers = row[4]
+            course_year = row[6]
+            data_filtered.append([institution, course_number, launch_date, course_title, teachers, course_year])
+        remove_dup(data_filtered)
+        filter_year(dup_free)
+        for row in dup_free:
+            print(row)
 
-def filter_year():
-    year = int(input('Vuoi filtrare i risultati per anno di corso? Se sì inserire un numero da 1 a 4, altrimenti inserire "0": '))
-    if year == 0:
+    elif instructor != '':     
+        while instructor != '':
+            instructors.append(instructor)
+            instructor = input('Vuoi inserire il nome di un altro professore? Inserire un altro nome oppure Invio per andare avanti: ')            
+        for row in data:
+            for instructor in instructors:
+                if instructor.lower().strip() in row[4].lower():
+                    institution = row[0]
+                    course_number = row[1]
+                    launch_date = row[2]
+                    course_title = row[3]
+                    teachers = row[4]
+                    course_year = row[6]
+                    data_filtered.append([institution, course_number, launch_date, course_title, teachers, course_year])
+        filter_year(data_filtered)
+        filter_date()
+    for row in data_filtered:
+        print(row)
+
+def filter_year(data):
+    year = str(input('Vuoi filtrare i risultati per anno di corso? Se sì inserire un numero da 1 a 4, altrimenti inserire "0": '))
+    if year == '0':
         return None
-    while year not in range(0,5):
-        year = int(input('Inserire un numero da 1 a 4: '))
-    
+    while year not in ['1', '2', '3', '4']:
+        year = str(input('Inserire un numero da 1 a 4: '))
+    for row in data:
+        if year != row[5]:
+            data.remove(row)
+    return data
+   
 def filter_date():
     date = int(input('Vuoi filtrare i risultati per anno solare? Se sì inserire un anno, altrimenti inserire "0": '))
     if date == 0:
@@ -46,9 +69,9 @@ def filter_date():
         date = int(input('Inserire un anno compreso tra 2012 e 2016: '))
 
 def remove_dup(data):
-    for x in data:
-        if x not in dup_free:
-            dup_free.append(x)
+    for row in data:
+        if row not in dup_free:
+            dup_free.append(row)
     return dup_free
 
 def main():
@@ -62,18 +85,6 @@ def main():
         print('Solo "u" o "l" sono risposte valide!')
         main()
 
+data_filtered = []
 dup_free = []
 main()
-filter_year()
-filter_date()
-
-# reader = csv.reader(lines)
-# header = next(reader)
-# for row in reader:
-#     institution = row[0]
-#     course_number = row[1]
-#     launch_date = row[2]
-#     course_title = row[3]
-#     instructors = row[4]
-#     year = int(row[6])
-#     data.append([institution, course_number, launch_date, course_title, instructors, year])
