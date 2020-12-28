643c7ef9a6a64e04257c1116835a4af28ed430b9
savag3s1nuso1d <lascagiovanni@outlook.it>
2020-12-28 09:47:18 +0100

    Task4 aggiornato e screen chiamata server postman

--- Task4/app.py
+++ Task4/app.py
@@ -29,7 +29,17 @@ def filter_instructor(instructor, data): #filtra per instructor
             course_title = row[3]
             teachers = row[4]
             course_year = row[6]
-            data_filtered.append([institution, course_number, launch_date, course_title, teachers, course_year])
+            # data_filtered.append([institution, course_number, launch_date, course_title, teachers, course_year])
+            data_filtered.append(
+                {
+                    "Institution": institution,
+                    "CourseNumber": course_number,
+                    "LaunchDate": launch_date,
+                    "CourseTitle": course_title,
+                    "Instructors": teachers,
+                    "Year": course_year
+                }
+            )
 
     return data_filtered
 
@@ -38,7 +48,9 @@ def filter_year(year, data): #filtra per anno di corso
         return data
     year_filtered = []
     for row in data:
-        if year == row[5]:
+        # if year == row[5]:
+        #     year_filtered.append(row)
+        if year == row.get('Year'):
             year_filtered.append(row)
     data = year_filtered
     return data
@@ -48,25 +60,17 @@ def filter_date(date, data): #filtra per anno solare
         return data
     date_filtered = []
     for row in data:
-        if row[2].endswith(date):
+        if row.get('LaunchDate').endswith(date):
             date_filtered.append(row)
     data = date_filtered
     return data
 
 @app.route('/') # Home
 def index():
-    return render_template('index.html')
+    return render_template('index.html')      
 
-@app.route('/datasubmit', methods = ["POST"]) # Crea json a partire dai dati inseriti nella form
-def datasubmit():
-    if request.method == 'POST':
-        f = request.form['csvfile']
-        local_method(f)
-        instructor = request.form['instructor']
-        launch_date = request.form['launch_date']
-        course_year = request.form['year']
-        json = '{ "instructor": "' + instructor + '", "launch_date": "'+ launch_date + '", "year": "'+ course_year + '" }'
-        return render_template('datasubmit.html', json = json)           
+# Non sono riuscito a fare in modo che la form passasse i dati in input al server in formato json, però la route del server accetta il json, filtra i risultati e restituisce un json come richiesto
+# Allego screen della chiamata al server fatto su Postman
 
 # webserver route
 @app.route('/server', methods = ['POST'])
@@ -77,18 +81,8 @@ def server():
     launch_date = data['launch_date']
     year = data['year']
     data_filtered = filter_date(launch_date, filter_year(year, filter_instructor(instructor, csvdata)))
-
-    result_json = '{ '
-
-    for row in data_filtered:
-        json += '{ "institution": ' + row[0] + ', "course_number": ' + row[1] + ', "launch_date": ' + row[2] + ', "course_title": ' + row[3] + ', "instructors": ' + row[4] + ', "course_year": ' + row[5] + '}, '
-
-    result_json += ' }'
-    return result_json
-
-# @app.route('/save', methods=['GET', 'POST']) # 3. Stampa risultati a schermo e possibilità di salvare in formato .xlsx
-# def save():
-#     return "File salvato correttamente!"
+    print(data_filtered)
+    return json.dumps(data_filtered)
 
 if __name__ == '__main__':
     app.run(debug=True)
